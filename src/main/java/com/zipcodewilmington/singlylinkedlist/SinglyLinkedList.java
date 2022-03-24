65e42d2ac9205cc2b76f32d0f2598b113f7b7f79
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-24 19:04:58 -0400

    add, get, and tests

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,89 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    private static int counter;
+    private Node head;
+
+    //constructor
+
+
+    public SinglyLinkedList() {
+    }
+
+    //adds element to end of list
+    public void add(Object data) {
+        //initialize node at head if one doesnt exist
+        if (head == null) {
+            head = new Node(data);
+        }
+        Node linkedTemp = new Node(data);
+        Node linkedCurrent = head;
+
+        if (linkedCurrent != null) {
+            //starts at head and goes to the end of the list and finds empty spot
+            while (linkedCurrent.getNext() != null) {
+                linkedCurrent = linkedCurrent.getNext();
+            }
+            //last node next reference set to new node
+            linkedCurrent.setNext(linkedTemp);
+        }
+        //adds to the number of elements
+        incrementCounter();
+    }
+
+    private static int getCounter() {
+        return counter;
+    }
+
+    private static void incrementCounter() {
+        counter++;
+    }
+
+    private static void decrementCounter() {
+        counter--;
+    }
+
+    //adds to a specific index
+    public void add(Object data, int index) {
+        Node linkedTemp = new Node(data);
+        Node linkedCurrent = head;
+
+        if (linkedCurrent != null) {
+            for (int i = 0; i < index && linkedCurrent.getNext() != null; i++) {
+                linkedCurrent = linkedCurrent.getNext();
+            }
+        }
+        linkedTemp.setNext(linkedCurrent.getNext());
+        linkedCurrent.setNext(linkedTemp);
+
+        incrementCounter();
+    }
+
+    //get method
+    public Object get(int index) {
+        if (index < 0) {
+            return null;
+        }
+        Node linkedCurrent = null;
+        if (head != null) {
+            linkedCurrent = head.getNext();
+            for (int i = 0; i < index; i++) {
+                if (linkedCurrent.getNext() == null) {
+                    return null;
+                }
+                linkedCurrent = linkedCurrent.getNext();
+
+                return linkedCurrent.getData();
+            }
+        }
+        return linkedCurrent;
+    }
+
 }
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -4,4 +4,28 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @org.junit.jupiter.api.Test
+    void addTest() {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        linkedList.add(3);
+        linkedList.add(4);
+        System.out.println(linkedList);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddtoIndexTest() {
+        SinglyLinkedList linkedList1 = new SinglyLinkedList();
+        linkedList1.add(0, 4);
+        linkedList1.add(1, 4);
+        System.out.println(linkedList1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTest() {
+        SinglyLinkedList linkedList2 = new SinglyLinkedList();
+        linkedList2.add(0, 4);
+        linkedList2.add(1, 4);
+        System.out.println(linkedList2.get(1));
+    }
 }
